64478db6161e418bb60d626fc2610840150e8015
Akhil Daver <164874270+projectdoon@users.noreply.github.com>
2024-08-03 21:15:29 +0530

    Update main.dart
    checking github again

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'my doon starting',
+      title: 'my doon start',
       theme: ThemeData(
         // This is the theme of your application.
         //
