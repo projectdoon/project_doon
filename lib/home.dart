8b755444da4df2f0d91afc551b8d9536f1734728
Akhil Daver <164874270+projectdoon@users.noreply.github.com>
2024-08-04 22:31:09 +0530

    location services added

--- lib/home.dart
+++ lib/home.dart
@@ -1,16 +1,105 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  final _NameController = TextEditingController();
+
+  late String latitude ;
+  late String longitude ;
+
+
+
+  @override
+  void dispose() {
+    _NameController.dispose();
+    super.dispose();
+  }
+
+  getCurrentLocation() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      print("Location denied");
+      LocationPermission ask = await Geolocator.requestPermission();
+    }
+
+    else {
+      Position currentPosition = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.best);
+      latitude=currentPosition.latitude.toString();
+      longitude=currentPosition.longitude.toString();
+      
+      print(latitude);
+      print(longitude);
+      setState(() {
+        LocationMessage="latitude = $latitude  longitude = $longitude";
+      });
+
+    }
+  }
+
+  String LocationMessage ='current location is';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: const Text('Register yourself'),
       ),
-      body: const Center(
-        child: Text('Welcome to the Home Page!'),
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 255, 255, 255),
+        ),
+        width: double.infinity,
+        height: double.infinity,
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              TextField(
+                controller: _NameController,
+                keyboardType: TextInputType.name,
+                autofocus: true,
+                decoration: const InputDecoration(labelText: 'Enter Your Name'),
+              ),
+              TextField(
+                controller: _NameController,
+                keyboardType: TextInputType.name,
+                autofocus: true,
+                decoration:
+                    const InputDecoration(labelText: 'Enter Your PhoneNumber'),
+              ),
+              const SizedBox(height: 20,),
+
+              Text(LocationMessage),
+
+
+
+              ElevatedButton(
+                onPressed: () {
+                  print('hii');
+                  getCurrentLocation();
+
+                },
+                child: const Text('detect my location'),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
